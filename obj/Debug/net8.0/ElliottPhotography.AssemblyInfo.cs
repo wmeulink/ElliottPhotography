c18c39a15d5c556dfdb0d977c18215e821ef2897
Whitney Meulink <wmeulink@gmail.com>
2025-10-03 17:30:04 -0700

    wip

--- obj/Debug/net8.0/ElliottPhotography.AssemblyInfo.cs
+++ obj/Debug/net8.0/ElliottPhotography.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElliottPhotography")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b621e0f669d986d0020a7f11eec92ac88d1bd735")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880e75d398b95bc0897d36ae5f310bfaaa330e33")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElliottPhotography")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElliottPhotography")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
